4cc61bddc86b847d05ccc6856061398f1a762b50
Sasha_Chemerchev <al_ch24@bk.ru>
2022-01-27 22:12:36 +0300

    Adding svg icons using webpack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logoSvg from "./assets/img/pizza-logo.svg";
 
 function App() {
   return (
@@ -6,7 +7,7 @@ function App() {
       <div class="header">
         <div class="container">
           <div class="header__logo">
-            <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+            <img width="38" src={logoSvg} alt="Pizza logo" />
             <div>
               <h1>React Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
